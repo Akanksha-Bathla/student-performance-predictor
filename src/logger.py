b3f68d6ecb8817d91d3dd143adc2b0752a3c126c
Akanksha Bathla <akankshabathla.18.22.26@gmail.com>
2026-07-21 05:16:50 +0530

    loggera and custom exception haddler is added

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os 
+from datetime import datetime
+
+LOG_DIR = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_DIR)
+os.makedirs(logs_path, exist_ok=True)
+
+log_file_path = os.path.join(logs_path, LOG_DIR)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
