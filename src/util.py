94cf7f659c80d08c6c78607fd40ba23af36f6f8c
Akanksha Bathla <akankshabathla.18.22.26@gmail.com>
2026-07-23 14:50:50 +0530

    predict pipeline is implemented

--- src/util.py
+++ src/util.py
@@ -1,12 +1,14 @@
 import os
+from pyexpat import model
 import sys
+from xml.parsers.expat import model
 import dill
 
 import pandas as pd
 import numpy as np
 
 from src.exception import CustomException
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import r2_score
 
 
@@ -22,16 +24,22 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
 
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            parameters = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, parameters, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            y_train_pred = gs.predict(X_train)
+            y_test_pred = gs.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
@@ -40,5 +48,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
